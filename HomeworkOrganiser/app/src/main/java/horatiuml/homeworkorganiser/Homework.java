30fc4ad113b2c5081ff166c53b89214a863320e2
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2017-01-15 18:17:03 +0200

    Created the Homework class

---
+++ HomeworkOrganiser/app/src/main/java/horatiuml/homeworkorganiser/Homework.java
@@ -0,0 +1,38 @@
+package horatiuml.homeworkorganiser;
+
+public class Homework {
+    private int id;
+    private String title;
+
+    public Homework(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String description;
+}
