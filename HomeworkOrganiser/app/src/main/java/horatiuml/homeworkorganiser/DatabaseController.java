47b6b6272df4216049bf15d48bccc31db061fc36
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2017-01-15 18:58:58 +0200

    Created a basic unimplemented db controller

---
+++ HomeworkOrganiser/app/src/main/java/horatiuml/homeworkorganiser/DatabaseController.java
@@ -0,0 +1,57 @@
+package horatiuml.homeworkorganiser;
+
+import java.util.ArrayList;
+
+public class DatabaseController {
+    public void addUser(User user) {
+        // TODO: Create logic
+    }
+
+    public void addHomework(Homework homework) {
+        // TODO: Create logic
+    }
+
+    public User getUser(String username, String password){
+        // TODO: Get logic
+
+        return null;
+    }
+
+    public ArrayList<User> getAllUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+
+        // TODO: Get logic
+
+        return users;
+    }
+
+    public User getHomework(String id){
+        // TODO: Get logic
+
+        return null;
+    }
+
+    public ArrayList<Homework> getAllHomeworks() {
+        ArrayList<Homework> homeworks = new ArrayList<Homework>();
+
+        // TODO: Get logic
+
+        return homeworks;
+    }
+
+    public void modifyUser(User user){
+        // TODO: Update logic
+    }
+
+    public void modifyHomework(Homework homework){
+        // TODO: Update logic
+    }
+
+    public void deleteUser(String id) {
+        // TODO: Delete logic
+    }
+
+    public void deleteHomework(String id) {
+        // TODO: Delete logic
+    }
+}
